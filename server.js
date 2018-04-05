0402f2d6768f4dd4b78641c5ec0cb66c9920de8c
Rafał Kowalczyk <34551849+Rafal-Kowalczyk@users.noreply.github.com>
2018-04-05 21:01:48 +0200

    Add connect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://rako.r.kowalczyk@interia.pl:rako25heroku/@ds137089.mlab.com:37089/mongork', {
     useMongoClient: true
 });
 
